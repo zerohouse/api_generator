9fbbb570a51301bd020b44ac2f3fa3517fe0930d
parks <parksungho86@gmail.com>
2026-02-22 16:31:21 +0900

    Bump version to 0.0.46 and enhance ApiGeneratorPlugin.
    Updated project version to 0.0.46 in build configurations and publication settings. Added `ApiGeneratorExtension` to support configurable `generateApi` task properties. Linked extension values (e.g., basePackage, outputPath) to the task for improved flexibility.

--- src/main/kotlin/com/github/zerohouse/api_generator/ApiGeneratorPlugin.kt
+++ src/main/kotlin/com/github/zerohouse/api_generator/ApiGeneratorPlugin.kt
@@ -5,7 +5,20 @@ import org.gradle.api.Project
 
 class ApiGeneratorPlugin : Plugin<Project> {
     override fun apply(project: Project) {
-        // 'generateApi'라는 이름의 태스크를 등록합니다.
-        project.tasks.register("generateApi", ApiGeneratorTask::class.java)
+        // 1. Extension 등록 (이게 있어야 tasks { } 밖에서 바로 쓸 수 있음)
+        val extension = project.extensions.create("generateApi", ApiGeneratorExtension::class.java)
+
+        // 2. 태스크 등록
+        project.tasks.register("generateApi", ApiGeneratorTask::class.java) { task ->
+            // Extension의 값을 태스크에 연결
+            task.basePackage = extension.basePackage
+            task.outputPath = extension.outputPath
+        }
     }
+}
+
+// 설정을 담을 데이터 클래스
+open class ApiGeneratorExtension {
+    var basePackage: String = ""
+    var outputPath: String = ""
 }
